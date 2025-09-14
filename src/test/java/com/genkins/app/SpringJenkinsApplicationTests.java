9492d5be0eb28703e23a7b8279ffbed576f86a9e
praveenkumarkambarlatest <praveenkumarkambar@gmail.com>
2025-09-14 11:47:15 +0530

    test case modified

--- src/test/java/com/genkins/app/SpringJenkinsApplicationTests.java
+++ src/test/java/com/genkins/app/SpringJenkinsApplicationTests.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 
@@ -13,6 +15,8 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("test case executing...");
+		logger.info("test case executing, second log statement...");
+		assertEquals(true, true);
 	}
 
 }
